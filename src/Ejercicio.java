56e5e1214c9920f645764b3843c3528788596639
unknown <garyaguilar300@gmail.com>
2021-10-20 10:30:10 -0400

    Se hizo fibonacci

--- src/Ejercicio.java
+++ src/Ejercicio.java
@@ -35,4 +35,23 @@ public class Ejercicio {
 
         return diferencia == 0;
     }
+    ////////////////////////////////////////////////////
+        /*public void main(String[] args) {
+            int maxCount = 10;
+            for (int i = 0; i <= maxCount; i++) {
+                int fibonacciNumber = printFibonacci(i);
+                System.out.print(" " + fibonacciNumber);
+            }
+        }*/
+
+        public static int printFibonacci(int n) {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return printFibonacci(n - 1) + printFibonacci(n - 2);
+        }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] a = {2, 9, 4};
+       /* int[] a = {2, 9, 4};
         int[] b = {2, 9, 4};
         boolean diferencia = Ejercicio.compararVectoresRecurcividad(a,b);
 
-        System.out.println(diferencia);
+        System.out.println(diferencia);*/
+       Scanner numero = new Scanner(System.in);
+        System.out.print("introducir numero a comvertir fibonaci : ");
+        int maxCount = numero.nextInt();
+        numero.close();
+
+        for (int i = 0; i < maxCount; i++) {
+            int fibonacciNumber = Ejercicio.printFibonacci(i);
+            System.out.print(" " + fibonacciNumber);
+        }
+
     }
+
 }
